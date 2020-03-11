cb2570329fe66e575f577c13a3f1060774ec6e56
Dmitry Maevsky <mart@MacBook-Pro-Dmitry.local>
2020-03-11 18:37:00 +0300

    Revert "neural"
    This reverts commit 87861109fc3c79799a2549553178c7278cccd068.

--- solution.py
+++ solution.py
@@ -1,44 +1,16 @@
 #!/usr/bin/python
-from __future__ import absolute_import, division, print_function, unicode_literals
 import sys
-
-
-import tensorflow as tf
+import pandas as pd
+import numpy as np
 
 def main():
     # print command line arguments
     input_dir, output_dir = sys.argv[1:]
-
-    mnist = tf.keras.datasets.mnist
-
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    # Build the `tf.keras.Sequential` model by stacking layers. Choose an optimizer and loss function for training:
-
-    # In[4]:
-
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
-
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    # Train and evaluate the model:
-
-    # In[5]:
-
-    model.fit(x_train, y_train, epochs=5)
-
-    model.evaluate(x_test, y_test, verbose=2)
-
-    model.save(output_dir+'/my_model.h5')
-
+    predicted_result = []
+    df = np.loadtxt(input_dir + '/data.data')
+    df = pd.DataFrame(df, columns=['column 1', 'column 2'])
+    df['result'] = df['column 1'] + df['column 2']
+    np.savetxt(output_dir + '/data.predict', np.array(df['result']))
     return 0
 
 if __name__ == "__main__":
